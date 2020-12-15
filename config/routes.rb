3af797d5414749949f336ea2025ab37f747b6ad9
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2020-12-15 14:15:19 +0900

    ユーザー、新規登録、ログイン、ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :items
 end
