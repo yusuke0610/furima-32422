c364db96b44fcf9a5edd95a6ae60ac46bea091c6
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2020-12-22 12:18:23 +0900

    商品詳細編集機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-   before_action :authenticate_user!, only: [:new]
+   before_action :authenticate_user!, only: [:new, :edit]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -20,9 +21,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit 
+    @item = Item.find(params[:id])
+    if @item.user == current_user
+    render :edit
+    else
+    redirect_to root_path
+    end
+  end
+
+  def update
+      @item = Item.find(params[:id])
+     if @item_update = @item.update(item_params)
+      redirect_to item_path(@item.id)
+     else
+      render :edit
+  end
+  end
 
   private
     def item_params
       params.require(:item).permit(:title, :explanation, :value ,:category_id ,:condition_id ,:delivery_fee_id ,:shipping_area_id ,:days_ship_id,:image ).merge(user_id: current_user.id)
     end
+
 end
