8658f564132a6046838b52c2101e6e66da9581fc
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2020-12-26 12:14:32 +0900

    商品購入機能完了1

---
+++ spec/factories/buyer_buy.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :buyer_buy do
+
+    postal_code { "123-1234" }
+    shipping_area_id { 2 }
+    city {"高知市"}
+    address {"高知1-2-3"}
+    phone_number { "12345678912" }
+    token {"tok_6ccef7b1a48cec3bc80e65102b39"}
+
+  end
+end
